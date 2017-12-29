53fbe29efa09616f7034c29c9d20e4698764bcc7
Gunter Rotsaert <gunter@mydeveloperplanet.com>
2017-12-29 14:30:51 +0100

    Added module com.mydeveloperplanet.jpmshi and add dependencies to module com.mydeveloperplanet.jpmshello

--- com.mydeveloperplanet.jpmshello/src/main/java/com/mydeveloperplanet/jpmshello/HelloModules.java
+++ com.mydeveloperplanet.jpmshello/src/main/java/com/mydeveloperplanet/jpmshello/HelloModules.java
@@ -1,5 +1,7 @@
 package com.mydeveloperplanet.jpmshello;
 
+import com.mydeveloperplanet.jpmshi.HiModules;
+
 import static javax.xml.XMLConstants.XML_NS_PREFIX;
 
 public class HelloModules {
@@ -7,6 +9,8 @@ public class HelloModules {
     public static void main(String[] args) {
         System.out.println("Hello Modules!");
         System.out.println("The XML namespace prefix is: " + XML_NS_PREFIX);
+        HiModules hiModules = new HiModules();
+        System.out.println(hiModules.getHi());
     }
 
 }
--- com.mydeveloperplanet.jpmshello/src/main/java/module-info.java
+++ com.mydeveloperplanet.jpmshello/src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module com.mydeveloperplanet.jpmshello {
     requires java.xml;
+    requires com.mydeveloperplanet.jpmshi;
 }
